c034b044faa4de31f37cca01c1eccb6d847a3240
Miracle Friday <miraclef60@gmail.com>
2023-09-25 09:44:19 +0100

    fix: fixed index, terms, and trade pages along with components on said pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +16,6 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    svelte(),
   ],
 });
