aa43fcd4eab8c114641605efc27e996ba39bac78
Miracle Friday <miraclef60@gmail.com>
2023-10-22 08:12:15 +0100

    chore: added the astrojs netlify package and cleaned up for deplyment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), react()],
+  output: "server",
+  adapter: netlify()
 });
