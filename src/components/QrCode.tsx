cd51e32996c95fa1ba2d7926f0cf38a042ca63d2
Miracle Friday <miraclef60@gmail.com>
2023-10-30 15:36:17 +0100

    fix: fixed QRCode component

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,25 +1,10 @@
 import { QRCodeSVG } from "qrcode.react";
-import { useEffect, useState } from "react";
 import speakeasy from "speakeasy";
 
 function QRCodeComp() {
-  const [secret, setSecret] = useState("secretestringdude");
-  useEffect(() => {
-    try {
-      const token = speakeasy.generateSecret({ length: 20 });
-      if (token) {
-        setSecret(token);
-      }
-    } catch (e: any) {
-      console.log(e.message ?? e.toString());
-    }
-  }, []);
-
-  // TODO: also save in the db as the users auth
-
   return (
     <div className="p-2 bg-white flex items-center justify-center">
-      <QRCodeSVG value={secret} size={180} />
+      <QRCodeSVG value={speakeasy.generateSecret({ length: 20 })} size={180} />
     </div>
   );
 }
